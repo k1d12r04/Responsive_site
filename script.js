4c7a1c808c29465727b483375f9fbcdc2adcd429
Kadir <k1d12r04@gmail.com>
2022-09-05 00:59:01 +0300

    Complete responsive site

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector(".nav");
+
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle("nav--visible");
+})
